0a393b16da7a5842dee4d1a78ed9f6338ebe5661
Hanyeop <super7615@naver.com>
2021-06-23 10:20:15 +0900

    뷰모델 구현

--- app/src/main/java/com/hanyeop/runnershigh/viewmodel/RunViewModel.kt
+++ app/src/main/java/com/hanyeop/runnershigh/viewmodel/RunViewModel.kt
@@ -1,8 +1,11 @@
 package com.hanyeop.runnershigh.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hanyeop.runnershigh.model.Run
 import com.hanyeop.runnershigh.repository.RunRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +13,11 @@ class RunViewModel @Inject constructor(
     private val runRepository: RunRepository
 ) : ViewModel() {
 
+    fun insertRun(run : Run) = viewModelScope.launch {
+        runRepository.insertRun(run)
+    }
+
+    fun deleteRun(run : Run) = viewModelScope.launch {
+        runRepository.deleteRun(run)
+    }
 }
