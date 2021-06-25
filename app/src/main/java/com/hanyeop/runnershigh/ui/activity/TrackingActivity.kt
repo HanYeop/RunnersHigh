c34bdb99dc1f8ea6909b10d7cf3c365596f53a8d
Hanyeop <super7615@naver.com>
2021-06-25 19:17:15 +0900

    구글 맵 뷰 연동

--- app/src/main/java/com/hanyeop/runnershigh/ui/activity/TrackingActivity.kt
+++ app/src/main/java/com/hanyeop/runnershigh/ui/activity/TrackingActivity.kt
@@ -2,11 +2,63 @@ package com.hanyeop.runnershigh.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hanyeop.runnershigh.R
+import com.google.android.gms.maps.GoogleMap
+import com.hanyeop.runnershigh.databinding.ActivityTrackingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TrackingActivity : AppCompatActivity() {
+
+    // ActivityTrackingBinding 선언
+    private lateinit var binding : ActivityTrackingBinding
+
+    // 구글맵 선언
+    private var map: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tracking)
+
+        // 뷰바인딩
+        binding = ActivityTrackingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            mapView.onCreate(savedInstanceState)
+            // 맵 불러오기
+            mapView.getMapAsync {
+                map = it
+            }
+        }
+    }
+
+
+
+
+    /**
+     * 라이프 사이클에 맞게 맵뷰를 처리해줌
+     */
+    override fun onResume() {
+        binding.mapView.onResume()
+        super.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
